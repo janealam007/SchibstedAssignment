03313d03db339980a0795e17e454dad8d9510031
jane_medinatech <jane@medinatech.co>
2022-03-15 01:59:05 +0200

    SchibstedApi interface class added and getPhotos api method declared with Querymap param

---
+++ app/src/main/java/com/tori/schibsted/data/remote/SchibstedApi.kt
@@ -0,0 +1,12 @@
+package com.tori.schibsted.data.remote
+
+import com.gaur.mealsearch.data.model.photo.PhotoResponseDto
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface SchibstedApi {
+    @GET("rest/")
+    suspend fun getPhotos(
+        @QueryMap options: Map<String, String>
+    ): PhotoResponseDto
+}
